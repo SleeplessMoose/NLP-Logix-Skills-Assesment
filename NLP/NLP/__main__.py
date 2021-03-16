40e8588611e5da5772a6c17de0d4929ecafc892a
SleeplessMoose <77353114+SleeplessMoose@users.noreply.github.com>
2021-03-16 11:14:48 -0400

    Create __main__.py

---
+++ NLP/NLP/__main__.py
@@ -0,0 +1,8 @@
+from __challenge1__ import challenge1
+from __challenge2__ import challenge2
+from __challenge5__ import challenge5
+
+if __name__ == '__main__':
+    challenge1()
+    challenge2()
+    challenge5()
